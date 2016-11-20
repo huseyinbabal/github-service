2cfcf0f3aea74fd50c55111147e5a55794e851a4
Huseyin BABAL <huseyinbabal88@gmail.com>
2016-11-20 22:07:17 +0300

    - Cors added

--- app.js
+++ app.js
@@ -4,6 +4,13 @@ var express = require('express'),
 
 app.use(bodyParser.json());
 
+app.use(function(req, res, next) {
+    res.header("Access-Control-Allow-Origin", "*");
+    res.header("Access-Control-Allow-Headers", "Origin, X-Requested-With, Content-Type, Accept");
+    res.header('Access-Control-Allow-Methods', 'OPTIONS, GET, POST, DELETE, PATCH, PUT');
+    next();
+});
+
 var repositoryController = require('./controller/repository');
 
 var router = express.Router();
